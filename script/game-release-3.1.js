4cadf7662402aa34a54b1da878edce6cbb1ce989
hec <chun.he@100credit.com>
2019-12-05 10:34:34 +0800

    删除分支之前的内容，重新编辑新的js文件

---
+++ script/game-release-3.1.js
@@ -0,0 +1,24 @@
+/**
+ * @author    hec9527
+ * @time      2019-12-5
+ * @change    2019-12-5
+ * @log
+ *    -  删除jquery库文件
+ *    -  删除撤销、排序按钮
+ */
+
+var a = 1;
+
+(function() {})();
+
+console.log(`
+
+@author  wenwen
+@time    2019-12-5
+@change  2019-12-5
+
+@log 
+
+    pen         
+
+`);
